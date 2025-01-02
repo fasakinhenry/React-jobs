77a34583dd330a92e533c23f784b0daf12e11403
Fasakin Henry <fasakinhenry@gmail.com>
2025-01-02 01:16:53 +0100

    chore: commit changes

--- src/pages/AddJobPage.jsx
+++ src/pages/AddJobPage.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+// Use react-auth toastgit add .
 import { toast } from 'react-toastify';
 
 const AddJobPage = ({ addJobSubmit }) => {
